4e0fc30ce4e89679428f0e0aff23ccf8b0371b2c
Milan van Wouden <milanvanwouden@live.nl>
2023-10-15 13:47:32 +0200

    celery add redis results backend

--- celery_playground/getting_started/main.py
+++ celery_playground/getting_started/main.py
@@ -3,4 +3,7 @@
 from celery_playground.getting_started.tasks import add
 
 if __name__ == "__main__":
-    add.delay(4, 4)
+    result = add.delay(4, 4)
+
+    print(f"Result ready: {result.ready()}")
+    print(f"Result: {result.get(timeout=5, propagate=False)}")
